eaf8511929d390a6fae6fff4d0fc6b83bd2d2b09
QSP <QSP@192.168.0.11>
2019-05-20 11:46:11 +0530

    First Commit

---
+++ src/main/java/com/spring/config/basics/App.java
@@ -0,0 +1,13 @@
+package com.spring.config.basics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
